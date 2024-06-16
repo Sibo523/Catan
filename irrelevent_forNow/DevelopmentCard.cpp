11f2785677252a45920118bc87a42ab871bd0c69
sibo <roi.siboni@msmail.ariel.ac.il>
2024-06-16 21:57:54 +0300

    created Card and got rid of the directory

--- irrelevent_forNow/DevelopmentCard.cpp
+++
@@ -1,8 +0,0 @@
-#include "DevelopmentCard.h"
-
-DevelopmentCard::DevelopmentCard(std::string cardType) 
-    : type(cardType) {}
-
-void DevelopmentCard::use() {
-    // Logic to use development card
-}
